015e389b91c16030bcbef26f2474fe8b4d79106a
Shah Dost <133655217+shahdostofficial@users.noreply.github.com>
2023-06-08 10:38:48 +0500

    Add files via upload

---
+++ Donations.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract Donations {
+    mapping(address => uint256) public amountDonated;
+
+    receive() external payable {
+        // your code here
+        // amountDonated should store the amount
+        // the person donated
+        // don't forget a person can donate
+        // multiple times!
+        amountDonated[msg.sender]+=msg.value;
+    }
+    
+}
